ea208551a786b182bfdad694f0b650abf76d3aaa
Fuego314 <l.tucker@hotmail.co.uk>
2017-02-03 16:19:07 +0000

    Update Examples Page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
-var React = require('react');
+const React = require('react');
+const {Link} = require('react-router');
 
 let Examples = props => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to examples page</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are a few example locations to try out</p>
+			<ol>
+				<li>
+					<Link to='/Cowplain'>Cowplain, GB</Link>
+				</li>
+				<li>
+					<Link to='/Rio'>Rio, Brazil</Link>
+				</li>
+			</ol>
 		</div>
 	);
 };
